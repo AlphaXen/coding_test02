af922ddac60448b5de5c3c799c9cac5e00fd2d2a
AlphaXen <kimminjae771@naver.com>
2025-10-30 15:01:11 +0900

    Updated Changes

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,10 +1,11 @@
-import 'package:flutter/material.dart;
+import 'package:flutter/material.dart;;
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-  
+    return Scaffold(
+    )
   }
 }
